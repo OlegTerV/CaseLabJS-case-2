0b1bf8a35374e4821657607181ec0648fcf45b40
OlegTerV <www.oleg2002ter@mail.ru>
2026-09-24 22:47:13 +0300

    feat: add security middleware
    Middleware with security headers for browser

---
+++ src/middlewares/securityMiddleWare.ts
@@ -0,0 +1,39 @@
+import type e = require("express")
+import nodeHttp = require("node:http")
+
+const express = require("express")
+const app = express()
+
+module.exports.securityHeaders = function (req: Request, res: Response & nodeHttp.ServerResponse, next: e.NextFunction) {
+    res.removeHeader("X-Powered-By")
+    res.setHeader(
+        "Content-Security-Policy",
+        `
+        default-src 'self';
+        script-src 'self';
+        style-src 'self' 'unsafe-inline';
+        img-src 'self' data: https:;
+        `
+    )
+    res.setHeader("X-Frame-Options", "DENY")
+    res.setHeader("X-Content-Type-Options", "nosniff")
+
+    /*
+    res.setHeader(
+        "Strict-Transport-Security",
+        `
+        max-age=3600;
+        includeSubDomains;
+        preload
+        `
+    )*/
+
+    res.setHeader("Referrer-Policy", "strict-origin-when-cross-origin")
+
+    res.setHeader(
+        "Permissions-Policy",
+        "geolocation=(), camera=(), microphone=()"
+    )
+
+    next()
+}
